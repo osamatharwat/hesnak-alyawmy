63cbe3d64f0035cd1c81dd2450acb3e010b108b3
osamatharwat <61668863+osamatharwat@users.noreply.github.com>
2026-05-07 14:51:10 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hesnak-cache-v1.8';
+const CACHE_NAME = 'hesnak-cache-v1.9';
 const urlsToCache = [
   './', './index.html', './style.css', './data.js', './app.js', './manifest.json', './icon-192.png', './icon-512.png'
 ];
